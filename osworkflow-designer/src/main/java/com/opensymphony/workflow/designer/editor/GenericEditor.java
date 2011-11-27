a5dfb62823a0d2c3fcabe7c3715abf6d748f294f
giuseppemiscione@gmail.com <giuseppemiscione@gmail.com@eac36e35-1ef6-d1b5-935b-798d758cd3c0>
2011-11-27 00:01:58 +0000

    OSWorkflow designer project
    git-svn-id: http://oswokflow-repository.googlecode.com/svn/trunk@25 eac36e35-1ef6-d1b5-935b-798d758cd3c0

---
+++ osworkflow-designer/src/main/java/com/opensymphony/workflow/designer/editor/GenericEditor.java
@@ -0,0 +1,74 @@
+package com.opensymphony.workflow.designer.editor;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import com.jgoodies.forms.builder.PanelBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+//import com.opensymphony.workflow.designer.UIFactory;
+//import com.opensymphony.workflow.designer.ResourceManager;
+//import com.opensymphony.workflow.designer.model.ResultsTableModel;
+
+public class GenericEditor extends DetailPanel
+{
+	protected JLabel label = new JLabel();
+
+	private boolean componentsInited = false;
+
+  public GenericEditor()
+  {
+  }
+
+  protected void initComponents()
+  {
+		String colLayout = "2dlu, max(32dlu;pref), 2dlu, pref:grow, 4dlu";
+		String rowLayout = "4dlu, pref, 2dlu, pref, 2dlu, pref, 2dlu, pref, 2dlu, pref";
+
+		JTabbedPane tabbedPane = new JTabbedPane();
+		CellConstraints cc = new CellConstraints();
+
+    FormLayout layout = new FormLayout("2dlu, 50dlu:grow, 2dlu", "2dlu, pref, 2dlu");
+    PanelBuilder builder = new PanelBuilder(layout, this);
+
+    /////////////////////////////////////////
+    // Tab1 (Info)
+    /////////////////////////////////////////
+		FormLayout layoutInfo = new FormLayout(colLayout, rowLayout);
+		JPanel panelInfo = new JPanel();
+		PanelBuilder builderInfo = new PanelBuilder(layoutInfo, panelInfo);
+    builderInfo.add(label, cc.xy(2, 2));
+    //builderInfo.addLabel(label, cc.xy(2, 2));
+
+    tabbedPane.add("Generic", panelInfo);
+
+    builder.add(tabbedPane, cc.xy(2,2));
+
+  }
+
+  public String getTitle()
+  {
+    return "Generic";
+  }
+
+	public final void setLabel(String text)
+	{
+		if(!componentsInited)
+		{
+			initComponents();
+			componentsInited = true;
+		}
+		label.setText(text);
+		updateView();
+	}
+
+	public String getLabel()
+	{
+		return label.getText();
+	}
+
+  protected void updateView()
+  {
+  }
+}
